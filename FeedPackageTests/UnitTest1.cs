8cc49f51169342fbcb485f1e79a93514e36782d1
Marcinkiewicz,Marcin M <m.marcinkiewicz@student.fontys.nl>
2023-03-31 12:05:37 +0200

    Added test libraries

---
+++ FeedPackageTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FeedPackageTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
