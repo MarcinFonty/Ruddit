8de4cb76dd3cd38bd26e70c4fbcdda55c1772d42
School <School@DESKTOP-LCC8D49>
2023-06-04 12:02:47 +0200

    Change Post Into PostModel and move to common for easier queryies.

---
+++ Common/DataModel/PostModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DataModel
+{
+    public class PostModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
--- Event_Handler_Service/Consumers/PostCreateEventConsumer.cs
+++ Event_Handler_Service/Consumers/PostCreateEventConsumer.cs
@@ -8,18 +8,10 @@ using Microsoft.Extensions.Configuration;
 using System.Xml.Linq;
 using System.IO;
 using System;
+using Common.DataModel;
 
 namespace Event_Handler_Service.Consumers
 {
-    public class Post
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string? Description { get; set; }
-        public string? ImagePath { get; set; }
-        public string Author { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
     public class PostCreateEventConsumer : AsyncEventingBasicConsumer
     {
         private readonly IConfiguration _configuration;
@@ -36,7 +28,7 @@ namespace Event_Handler_Service.Consumers
             var json = Encoding.UTF8.GetString(body.ToArray());
             var postCreateEvent = JsonSerializer.Deserialize<PostCreateEvent>(json);
 
-            Post result = new Post
+            PostModel result = new PostModel
             {
                 Id = postCreateEvent.Id,
                 Title = postCreateEvent.Title,
@@ -50,7 +42,7 @@ namespace Event_Handler_Service.Consumers
             var mongoClient = new MongoClient(_configuration["MongoDB:ConnectionString"]);
             var mongoDatabase = mongoClient.GetDatabase(_configuration["MongoDB:DatabaseName"]);
 
-            var collection = mongoDatabase.GetCollection<Post>("Posts");
+            var collection = mongoDatabase.GetCollection<PostModel>("Posts");
             collection.InsertOne(result);
 
             Model.BasicAck(deliveryTag, false);
