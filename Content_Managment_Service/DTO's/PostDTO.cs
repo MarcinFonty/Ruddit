b1eb5064d2991a603960acf04f07c0e92c652315
School <School@DESKTOP-LCC8D49>
2023-06-18 05:10:15 +0200

    Updated post DTO to be nullable and docker compose to include Owasp Zap

--- Content_Managment_Service/DTO's/PostDTO.cs
+++ Content_Managment_Service/DTO's/PostDTO.cs
@@ -5,8 +5,8 @@ namespace Content_Managment_Service.DTO_s
     public class PostDTO
     {
         public string Title { get; set; }
-        public string Description { get; set; }
-        public string ImagePath { get; set; }
+        public string? Description { get; set; }
+        public string? ImagePath { get; set; }
         public string Author { get; set; }
     }
 }
